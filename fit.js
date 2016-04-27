3263872b2f3d9d1090e9f1f8c865fa4fb1303da5
Daniel Carlsson <daniel.carlsson.1987@gmail.com>
2016-04-27 11:14:22 +0200

    Refactor fit algorithm
    No better, no worse, just different

--- fit.js
+++ fit.js
@@ -5,24 +5,13 @@
  */
 
 module.exports = function(rect, bounds) {
-    var rectRatio = rect.width / rect.height;
-    var boundsRatio = bounds.width / bounds.height;
-
-    var newRect = {};
-
-    if (rectRatio > boundsRatio) {
-
-        // Rect is more landscape than bounds - fit to width
-        newRect.width = bounds.width;
-        newRect.height = rect.height * (bounds.width / rect.width);
-
-    } else {
-
-        // Rect is more portrait than bounds - fit to height
-        newRect.width = rect.width * (bounds.height / rect.height);
-        newRect.height = bounds.height;
-
-    }
-
-    return newRect;
+    
+    // Find smallest acceptable ratio
+    var ratio = Math.min(bounds.width / rect.width, bounds.height / rect.height, 1);
+    
+    // Multiply the height and width with the ratio
+    return {
+        height: rect.height * ratio,
+        width: rect.width * ratio
+    };
 };
